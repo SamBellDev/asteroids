95b3b2b086dad366091fd32d39838c9f7e358d16
SamBellDev <sambelldev@tuta.io>
2024-12-26 20:57:43 +0000

    Add clock to main.py and create new CircleShape class as a base for our in game objects

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
 
