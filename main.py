09a88b5c6a1aac4e83a5e501e87f32577cae2fb3
SamBellDev <sambelldev@tuta.io>
2024-12-31 19:47:55 +0000

    Update main to utilise drawable and updateable groups so we can organise more objects.

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     pygame.init()
+
+    drawable = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Player.containers = (drawable, updateable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
 
     while True:
         for event in pygame.event.get():
@@ -20,8 +25,10 @@ def main():
                 return
         dt = clock.tick(60) / 1000
         pygame.Surface.fill(screen, (0,0,0)) 
-        player.draw(screen)
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for item in updateable:
+            item.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
